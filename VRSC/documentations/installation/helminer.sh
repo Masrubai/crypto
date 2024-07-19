5f898634400e014d486a11b817e4806397bacd23
Mas Rubai <masrubai@gmail.com>
2024-07-20 04:22:30 +0700

    Update helminer.sh

--- VRSC/documentations/installation/helminer.sh
+++ VRSC/documentations/installation/helminer.sh
@@ -11,21 +11,21 @@ mkdir helminer && cd helminer
 # Clone miner from github repository
 wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64_avx2.tar.gz
 
-# Tunggu proses selesai dan ekstrak file tar.gz
+# Waiting procces done & extract file tar.gz
 if [ $? -eq 0 ]; then
   tar -xzvf hellminer_linux64_avx2.tar.gz
   if [ $? -eq 0 ]; then
     # Edit file run_miner.sh
     echo "./hellminer -c stratum+tcp://paddypool.net:9999 -u RKdmLwRbR7riY543AfsYqPFnMaZfeZNDRR.Worker3 -p x --cpu 2" > run_miner.sh
     
-    # Berikan izin eksekusi pada file run_miner.sh
+    # Give permission allow at file run_miner.sh
     chmod +x run_miner.sh
     # Konfirmasi tugas selesai
-    echo "Tugas selesai. Script telah berhasil dijalankan."
+    echo "Jobs Success, the script has been successfully executed."
   else
-    echo "Gagal mengekstrak file tar.gz"
+    echo "Fail extract file tar.gz"
   fi
 else
-  echo "Gagal mengunduh file dari github"
+  echo "Fail download from github"
 fi
 bash run_miner.sh
