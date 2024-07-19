d3965c249e01e93014682300494dab0deab02280
Mas Rubai <masrubai@gmail.com>
2024-07-20 04:31:15 +0700

    Update ccminer.sh

--- VRSC/documentations/installation/ccminer.sh
+++ VRSC/documentations/installation/ccminer.sh
@@ -1,1 +1,4 @@
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
 wget https://github.com/Oink70/ccminer-verus/archive/refs/tags/v3.8.3a-CPU.tar.gz
+chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh
+ ./build.sh
