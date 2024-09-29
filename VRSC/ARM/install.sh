bfc0beb927040a299cc3253847203fe2041a8d8a
Mas Rubai <masrubai@gmail.com>
2024-09-30 00:10:45 +0700

    Update install.sh

--- VRSC/ARM/install.sh
+++ VRSC/ARM/install.sh
@@ -1,13 +1,11 @@
 apt update && apt upgrade  
 
-sudo apt-get install git gcc-5 g++-5 libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential 
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 1
-git clone https://github.com/monkins1010/ccminer.git
+sudo apt-get install git libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential 
+wget https://github.com/Oink70/ccminer-verus/archive/refs/tags/v3.8.3a-CPU.tar.gz
+tar -xzvf v3.8.3a-CPU.tar.gz && mv ccminer-verus-3.8.3a-CPU /ccminer
 #git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
 cd ccminer
 chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh 
-./autogen
-./configure
 ./build.sh 
 touch run_miner.sh && chmod +x run_miner.sh
 echo "./ccminer -a verus  -o stratum+tcp://sg.vipor.net:5040 -u RKdmLwRbR7riY543AfsYqPFnMaZfeZNDRR.SID01  -p d=6 -t 4" > run_miner.sh
