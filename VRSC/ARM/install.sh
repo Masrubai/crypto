8fedf4825bb022af7c31df287afe8e57a18c3c9d
Mas Rubai <masrubai@gmail.com>
2024-09-30 00:38:12 +0700

    Update install.sh

--- VRSC/ARM/install.sh
+++ VRSC/ARM/install.sh
@@ -1,9 +1,7 @@
 apt update && apt upgrade  
 
 sudo apt-get install git libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential 
-wget https://github.com/Oink70/ccminer-verus/archive/refs/tags/v3.8.3a-CPU.tar.gz
-tar -xzvf v3.8.3a-CPU.tar.gz && mv ccminer-verus-3.8.3a-CPU /ccminer
-#git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
 cd ccminer
 chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh 
 ./build.sh 
