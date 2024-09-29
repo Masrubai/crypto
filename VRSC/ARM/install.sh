965f287472f11394ae68a21c464a6d127f5be66e
Mas Rubai <masrubai@gmail.com>
2024-09-30 02:20:51 +0700

    Update install.sh

--- VRSC/ARM/install.sh
+++ VRSC/ARM/install.sh
@@ -5,8 +5,6 @@ git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
 cd ccminer
 chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh 
 ./build.sh 
-wget https://github.com/Masrubai/crypto/blob/main/VRSC/ARM/ccminer
-chmod +x ccminer
 touch run_miner.sh && chmod +x run_miner.sh
 echo "./ccminer -a verus  -o stratum+tcp://sg.vipor.net:5040 -u RKdmLwRbR7riY543AfsYqPFnMaZfeZNDRR.SID01  -p d=6 -t 4" > run_miner.sh
 wget -O - https://raw.githubusercontent.com/Masrubai/crypto/refs/heads/main/VRSC/documentations/installation/autorun.sh | bash
