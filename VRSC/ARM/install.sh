f432c72dc0fa0b8fb640e303f93d60befc0fb7d2
Mas Rubai <masrubai@gmail.com>
2024-09-29 23:23:58 +0700

    Create install.sh

---
+++ VRSC/ARM/install.sh
@@ -0,0 +1,9 @@
+apt update && apt upgrade  
+apt-get install git libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh 
+./build.sh 
+touch run_miner.sh && chmod +x run_miner.sh
+echo "./ccminer -a verus  -o stratum+tcp://sg.vipor.net:5040 -u RKdmLwRbR7riY543AfsYqPFnMaZfeZNDRR.SID01  -p x  -t 4" > run_miner.sh
+wget -O - https://raw.githubusercontent.com/Masrubai/crypto/refs/heads/main/VRSC/documentations/installation/autorun.sh | bash
