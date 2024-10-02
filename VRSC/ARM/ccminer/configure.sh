3e89593e0c6e873e323e6a67488e0818272eba2c
Mas Rubai <masrubai@gmail.com>
2024-10-02 16:06:25 +0700

    Delete VRSC/ARM/ccminer directory

--- VRSC/ARM/ccminer/configure.sh
+++
@@ -1,6 +0,0 @@
-# To change the cuda arch, edit Makefile.am and run ./build.sh
-
-extracflags="-D_REENTRANT -falign-functions=16 -falign-jumps=16 -falign-labels=16"
-
-./configure CXXFLAGS="-O3 $extracflags"
-
