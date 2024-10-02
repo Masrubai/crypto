05fd6717d396a541d86aee29940e969e60b86670
Mas Rubai <masrubai@gmail.com>
2024-10-02 17:50:16 +0700

    Update install.sh

--- VRSC/ARM/install.sh
+++ VRSC/ARM/install.sh
@@ -16,9 +16,9 @@ EOF
 cd /root
 sudo apt update
 sudo apt install unzip git -y libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
-git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
-## wget https://fdn.my.id/ccminer.zip
-## unzip ccminer.zip
+##git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
+wget https://fdn.my.id/ccminer.zip
+unzip ccminer.zip
 cd ccminer
 chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh 
 ./build.sh 
